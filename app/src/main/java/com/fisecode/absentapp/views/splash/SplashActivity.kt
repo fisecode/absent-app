9f9c5096f55108661e5afdf3f20742cd9a930bc5
fisecode <akhmatfikriseptiawan@gmail.com>
2022-05-30 16:53:15 +0700

    ui: add login page

--- app/src/main/java/com/fisecode/absentapp/views/splash/SplashActivity.kt
+++ app/src/main/java/com/fisecode/absentapp/views/splash/SplashActivity.kt
@@ -2,11 +2,24 @@ package com.fisecode.absentapp.views.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.fisecode.absentapp.MainActivity
 import com.fisecode.absentapp.R
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        afterDelayGoToLogin()
+    }
+
+    private fun afterDelayGoToLogin() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity<MainActivity>()
+            finishAffinity()
+        }, 1200)
     }
 }
