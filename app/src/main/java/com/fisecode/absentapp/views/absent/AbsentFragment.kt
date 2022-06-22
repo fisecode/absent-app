eab368dcdeab09278ef293d59bd1fed9958f3de3
Fisecode <akhmatfikriseptiawan@gmail.com>
2022-06-23 06:14:45 +0700

    Add ui-history

--- app/src/main/java/com/fisecode/absentapp/views/absent/AbsentFragment.kt
+++ app/src/main/java/com/fisecode/absentapp/views/absent/AbsentFragment.kt
@@ -6,15 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.fisecode.absentapp.R
+import com.fisecode.absentapp.databinding.FragmentAbsentBinding
+import com.fisecode.absentapp.views.changepassword.ChangePasswordActivity
+import com.fisecode.absentapp.views.history.HistoryActivity
+import org.jetbrains.anko.startActivity
 
 class AbsentFragment : Fragment() {
+
+    private var binding: FragmentAbsentBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_absent, container, false)
+        binding = FragmentAbsentBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClick()
+    }
+
+    private fun onClick() {
+        binding?.btnHistory?.setOnClickListener {
+            context?.startActivity<HistoryActivity>()
+        }
     }
 
 }
