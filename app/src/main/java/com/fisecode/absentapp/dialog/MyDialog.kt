d0d63fc791227dea3851f496533a7d81dbb7fce1
Fisecode <akhmatfikriseptiawan@gmail.com>
2022-06-27 02:41:23 +0700

    add custom dialog

---
+++ app/src/main/java/com/fisecode/absentapp/dialog/MyDialog.kt
@@ -0,0 +1,35 @@
+package com.fisecode.absentapp.dialog
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import com.fisecode.absentapp.R
+
+object MyDialog {
+
+    private var dialogBuilder: AlertDialog? = null
+
+    fun dynamicDialog(context: Context?, title: String, message: String){
+        dialogBuilder = AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .show()
+    }
+
+    fun showProgressDialog(context: Context?){
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.layout_progress, null)
+        dialogBuilder = AlertDialog.Builder(context)
+            .setView(dialogView)
+            .setCancelable(false)
+            .create()
+
+        dialogBuilder?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialogBuilder?.show()
+    }
+
+    fun hideDialog(){
+        if (dialogBuilder != null) {
+            dialogBuilder?.dismiss()
+        }
+    }
+}
