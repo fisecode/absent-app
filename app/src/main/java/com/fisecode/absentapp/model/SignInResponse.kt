8f76798bd014bfc80e06d35b1d7a772ec0a476ab
Fisecode <akhmatfikriseptiawan@gmail.com>
2022-06-28 11:43:44 +0700

    Update Test 2

--- app/src/main/java/com/fisecode/absentapp/model/SignInResponse.kt
+++ app/src/main/java/com/fisecode/absentapp/model/SignInResponse.kt
@@ -4,8 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class SignInResponse(
 
-	val aa: Nothing? = null,
-
 	@field:SerializedName("access_token")
 	val accessToken: String? = null,
 
