bb47783c20854b9b86973e7bc1ed7c7ff22bb511
Fisecode <akhmatfikriseptiawan@gmail.com>
2022-06-28 12:11:22 +0700

    Update 3

--- app/src/main/java/com/fisecode/absentapp/model/Leave.kt
+++ app/src/main/java/com/fisecode/absentapp/model/Leave.kt
@@ -1,4 +1,14 @@
 package com.fisecode.absentapp.model
 
+import com.google.gson.annotations.SerializedName
+
 class Leave {
+    @field:SerializedName("code")
+    val code: Int? = null
+
+    @field:SerializedName("message")
+    val message: String? = null
+
+    @field:SerializedName("status")
+    val status: String? = null
 }
